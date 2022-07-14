8d5aef47c7ed0c38b09d21d2b31837b3f839980d
Fatemeh Heydari <96463991+rojjhan@users.noreply.github.com>
2022-07-14 13:06:18 +0430

    Add files via upload

---
+++ condition_practice2.py
@@ -0,0 +1,12 @@
+#win the race
+
+myRank = 2
+
+if myRank == 1:
+    print("You got GOLD medal")
+elif myRank == 2:
+    print("You got SILVER medal")
+elif myRank == 3:
+    print("You got BORONZ medal")
+else:
+    print("You got no medal")
