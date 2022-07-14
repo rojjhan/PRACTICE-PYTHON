cd4add075c6e2660bfb0a0ed805cec9725e44e2c
Fatemeh Heydari <96463991+rojjhan@users.noreply.github.com>
2022-07-14 13:31:16 +0430

    Update condition_practice1.py

--- condition_practice1.py
+++ condition_practice1.py
@@ -4,3 +4,20 @@ if numb % 3 == 0 :
     print(f"The number { numb } is divisible by 3")
 else:
     print(f"The number {numb} is not divisible by 3")
+    
+    
+    
+#win the race
+
+myRank = 2
+
+if myRank == 1:
+    print("You got GOLD medal")
+elif myRank == 2:
+    print("You got SILVER medal")
+elif myRank == 3:
+    print("You got BORONZ medal")
+else:
+    print("You got no medal")
+    
+print("You win this race") if myRank == 1 else print("You dont win")
