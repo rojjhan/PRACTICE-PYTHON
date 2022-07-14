4f18c3a95c2e6815c6f120a7f4cebd9f62398dba
Fatemeh Heydari <96463991+rojjhan@users.noreply.github.com>
2022-07-14 12:48:46 +0430

    Add files via upload
    This program check divisible numbers by 3

---
+++ condition_practice1.py
@@ -0,0 +1,6 @@
+#divisibility of the number by 3
+numb = int(input('enter number:'))
+if numb % 3 == 0 :
+    print(f"The number { numb } is divisible by 3")
+else:
+    print(f"The number {numb} is not divisible by 3")
